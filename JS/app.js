2eb1d0ec3447efb11c4566072d764cf46d0ea2fd
Fokhrul Islam <shahidcse12@gmail.com>
2022-03-01 02:29:42 +0600

    button-handler-added

--- JS/app.js
+++ JS/app.js
@@ -1,34 +1,54 @@
 
 
-const searchPhone = () =>{
+const searchPhone = () => {
     //console.log("button clicked");
 
     const searchField = document.getElementById('search-field');
     const errorMessege = document.getElementById('error-messege');
-   const searchText = searchField.value;
-   console.log(searchText);
+    const searchText = searchField.value;
+    console.log(searchText);
 
- if(searchText=='iphone' || searchText=='samsung'|| searchText=='oppo' || searchText=='huawei' ){
-    //  console.log('Phone Found');
+    if (searchText == 'iphone' || searchText == 'samsung' || searchText == 'oppo' || searchText == 'huawei') {
+        //  console.log('Phone Found');
 
-     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    fetch(url)
-    .then(res=>res.json())
-    .then (data=>phoneInfo(data))
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => phoneInfo(data.data))
 
 
-    searchField.value='';
-    
- }
+        searchField.value = '';
 
- else{
-    errorMessege.innerText ="Sorry, Your Desired Phone Is Not Found"
-    searchField.value='';
- }
+    }
+
+    else {
+        errorMessege.innerText = "Sorry, Your Desired Phone Is Not Found"
+        searchField.value = '';
+    }
 
 }
 
-const phoneInfo = (phone) =>{
+const mainDiv = document.getElementById('main-div');
+
+const phoneInfo = (phone) => {
     console.log(phone);
+    phone.forEach(phoneList => {
+        //console.log(phoneList.image);
+        const div = document.createElement('div');
+        div.classList.add('col-lg-4')
+        div.innerHTML = `
+        <div class="card" style="width: 18rem;">
+             <img src="${phoneList.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${phoneList.phone_name}</h5>
+            <p>Brand: ${phoneList.brand}<p>
+            <button href="#" class="btn btn-primary">See Details</button>
+        </div>
+        </div>
+        
+        `
+        mainDiv.appendChild(div);
+
+    })
 
 }
